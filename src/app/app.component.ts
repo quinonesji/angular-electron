67715f83c0d9c6a7b1f327d9c5a9f63864ba762f
Jose Quinones <quinonesji@knights.ucf.edu>
2021-02-10 19:30:12 -0500

    adding electron timer feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { takeWhile, map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-electron';
+
+  max     = 1;
+  current = 0;
+
+  start() {
+    
+    interval(100)
+      .pipe(takeWhile(_ => !this.isFinished ))
+      .pipe(map(i => this.current += 0.1))
+      .subscribe();
+  }
+
+   /// finish timer
+  finish() {
+    this.current = this.max;
+  }
+
+  /// reset timer
+  reset() {
+    this.current = 0;
+  }
+
+
+  /// Getters to prevent NaN errors
+
+  get maxVal() {
+    return isNaN(this.max) || this.max < 0.1 ? 0.1 : this.max;
+  }
+
+  get currentVal() {
+    return isNaN(this.current) || this.current < 0 ? 0 : this.current;
+  }
+
+  get isFinished() {
+    return this.currentVal >= this.maxVal;
+  }
+
+
+
 }
